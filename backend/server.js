b7a3d47d50350f7cd9bcb5f3a1d5e2c0ccb0fb65
Shubham Gupta <sg350348@gmail.com>
2022-03-28 10:56:35 +0530

    making deployment ready

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const passportConfig = require("./lib/passportConfig");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 
 // MongoDB
 mongoose
@@ -44,6 +45,10 @@ app.use("/api", require("./routes/apiRoutes"));
 app.use("/upload", require("./routes/uploadRoutes"));
 app.use("/host", require("./routes/downloadRoutes"));
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
 });
